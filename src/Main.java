4717741ece99e66e0cde30154ed87a9c8de38487
Kashif Nazar <kashifnazar278@gmail.com>
2025-08-11 18:02:12 +0500

    Some Improvemnt

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,26 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-
-//        Java Program to create signup for banks for users if different age and account
+        // Java Program to create signup for banks for users of different age and account types
 
         Scanner age = new Scanner(System.in);
         System.out.println("Enter your Age");
         int yourAge = age.nextInt();
 
-
         if (yourAge < 21) {
-
             System.out.println("You are not eligible");
-
-        }
-        else if (yourAge > 21 && yourAge < 55){
-            System.out.println("Savings Account ");
-        }
-        else if (yourAge > 55 ){
-
-            System.out.println("Senior Citizen Account ");
         }
 
-
-
-
-
-
-
+        if (yourAge >= 21) {
+            if (yourAge > 55) {
+                System.out.println("Senior Citizen");
+            } else if (yourAge <55) {
+                System.out.println("Savings Account");
+            }
         }
+
+        age.close(); // always good practice to close the Scanner
     }
+}
